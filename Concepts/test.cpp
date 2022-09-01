612dffc9a36b335d7b39b45ed8f531a4a4fc4346
Ediblelnk <peters1731@gmail.com>
2022-08-31 21:41:28 -0400

    First stuff made

---
+++ Concepts/test.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    string inp;
+    cout << "Say something!\n";
+    cin >> inp;
+    cout << "Why did you say " << inp << "?";
+    return 0;
+}
